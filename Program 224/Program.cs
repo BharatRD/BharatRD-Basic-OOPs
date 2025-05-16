e22a55cce2b0477638e273abb7d0a64b98cf89bb
Bharat RD <bharatrddev@gmail.com>
2025-05-17 00:05:37 +0530

    Author fixed

---
+++ Program 224/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Transactions;
+namespace OOPs
+{
+    //Creating Class
+    class Student
+    {
+        //Creating Public Variable in student class to store Data
+        public string name;
+        public int rollnum;
+        public float marks;
+
+        //Printing the output Function
+        public void Display()
+        {
+            Console.WriteLine("Name Of Student - " + name);
+            Console.WriteLine("Roll Number - " + rollnum);
+            Console.WriteLine("Marks - "+ marks);
+        }
+        
+    }
+    class Display
+    {
+        static void Main()
+        {
+            //Creating Object named obj1 (Converting student class in Object obj1)
+            Student obj1 = new Student();
+           
+            //storing values in variables declared in class student
+            Console.WriteLine("Enter Student name: ");
+            obj1.name = Console.ReadLine();
+
+            Console.WriteLine("Enter Roll Number");
+            obj1.rollnum = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Marks- ");
+            obj1.marks = float.Parse(Console.ReadLine());
+
+            //calling function to print stored Data
+            Console.WriteLine("-----------Student Details----------");
+            obj1.Display();
+            
+            
+
+        }
+    }
+}
